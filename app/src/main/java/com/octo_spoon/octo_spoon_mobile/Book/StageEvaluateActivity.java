8c8efc65b9a50d71a99a64eb0e1216ae26a46ef3
Esteban Miño <efmino@uc.cl>
2017-11-04 00:19:27 -0300

    evaluate stage with tabbed fragments

--- app/src/main/java/com/octo_spoon/octo_spoon_mobile/Book/StageEvaluateActivity.java
+++ app/src/main/java/com/octo_spoon/octo_spoon_mobile/Book/StageEvaluateActivity.java
@@ -3,6 +3,11 @@ package com.octo_spoon.octo_spoon_mobile.Book;
 import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -13,6 +18,8 @@ import com.octo_spoon.octo_spoon_mobile.R;
 public class StageEvaluateActivity extends AppCompatActivity {
 
     private FloatingActionButton fabToCommunicate;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,16 @@ public class StageEvaluateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stage_evaluate);
 
         getSupportActionBar().setTitle("Evaluar");
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
         fabToCommunicate = (FloatingActionButton) findViewById(R.id.fab_to_communicate);
         fabToCommunicate.setOnClickListener(new View.OnClickListener() {
@@ -28,10 +45,46 @@ public class StageEvaluateActivity extends AppCompatActivity {
                 startActivity(StageCommunicateActivity.getIntent(StageEvaluateActivity.this));
             }
         });
+
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch  (position) {
+                case 0:
+                    return new EvaluateProcessFragment();
+                case 1:
+                    return new EvaluateQuestionsFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Proceso";
+                case 1:
+                    return "Consultas a Usuarios";
+            }
+            return null;
+        }
     }
 
     public static Intent getIntent(Context context) {
         Intent intent = new Intent(context,StageEvaluateActivity.class);
         return intent;
     }
+
 }
