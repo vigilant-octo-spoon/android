dddf4e7f7c74bcff50dac745ebee7200e26b2b81
Esteban Miño <efmino@uc.cl>
2017-11-07 20:40:20 -0300

    stage implementation get edittext

--- app/src/main/java/com/octo_spoon/octo_spoon_mobile/Book/StageImplementationActivity.java
+++ app/src/main/java/com/octo_spoon/octo_spoon_mobile/Book/StageImplementationActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 import com.octo_spoon.octo_spoon_mobile.R;
 
@@ -14,16 +15,35 @@ public class StageImplementationActivity extends AppCompatActivity {
 
     private FloatingActionButton fabToEvaluate;
 
+
+    private EditText editFromDate;
+    private EditText editDates;
+    private EditText editFinalDate;
+    private EditText editObservations;
+    private EditText editAdvances;
+    private EditText editObstacles;
+    private EditText editNewIdeas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stage_implementation);
         getSupportActionBar().setTitle("Implementar");
 
+        editFromDate = (EditText) findViewById(R.id.edit_initial_date);
+        editDates = (EditText) findViewById(R.id.edit_dates);
+        editFinalDate = (EditText) findViewById(R.id.edit_final_date);
+        editObservations = (EditText) findViewById(R.id.edit_observations);
+        editAdvances = (EditText) findViewById(R.id.edit_advances);
+        editObstacles = (EditText) findViewById(R.id.edit_obstacles);
+        editNewIdeas = (EditText) findViewById(R.id.edit_new_ideas);
+
         fabToEvaluate = (FloatingActionButton) findViewById(R.id.fab_to_evaluate);
         fabToEvaluate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // TODO: 07-11-2017 IMPLEMENTATION REQUEST
                 startActivity(StageEvaluateActivity.getIntent(StageImplementationActivity.this));
             }
         });
